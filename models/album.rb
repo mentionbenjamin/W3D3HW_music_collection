a44747711cbdc168f7e781991dcd18c95f2e9342
Ben Robinson <mentionbenjamin@gmail.com>
2018-06-06 15:33:07 +0100

    classes setup

--- models/album.rb
+++ models/album.rb
@@ -3,4 +3,11 @@ require_relative("../db/sql_runner.rb")
 
 class Album
 
+  def initialize(details)
+    @id = details["id"].to_i() if details["id"]
+    @name = details["name"]
+    @genre = details["genre"]
+    @artist_id = details["artist_id"].to_i()
+  end
+
 end
--- models/artist.rb
+++ models/artist.rb
@@ -2,5 +2,10 @@ require("pg")
 require_relative("../db/sql_runner.rb")
 
 class Artists
-  
+
+  def initialize(details)
+    @id = details["id"].to_i() if details["id"]
+    @name = details["name"]
+  end
+
 end
